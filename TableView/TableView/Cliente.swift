aaebcb5d88100ce35a94a4307dcdfdc5a8429da7
MigueAJM9724 <miguejm@outlook.es>
2020-02-21 02:03:09 -0800

    Ejemplo de TableView y Ejemplo TableView con IMC

---
+++ TableView/TableView/Cliente.swift
@@ -0,0 +1,9 @@
+//
+//  Cliente.swift
+//  TableView
+//
+//  Created by MIGUEL ANGEL JIMENEZ MELENDEZ on 21/02/20.
+//  Copyright © 2020 MIGUEL ANGEL JIMENEZ MELENDEZ. All rights reserved.
+//
+
+import Foundation
---
+++ TableView/TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by MIGUEL ANGEL JIMENEZ MELENDEZ on 21/02/20.
+//  Copyright © 2020 MIGUEL ANGEL JIMENEZ MELENDEZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
